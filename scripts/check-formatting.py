7d5b680006af02e0469921d7a8b9fdaefb6f32a6
Aurelien <aurelien.laporte@canaltp.fr>
2019-01-04 16:13:31 +0100

    Adding pre-commit hook and check-formatting script for futur CI

---
+++ scripts/check-formatting.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+import subprocess
+output = subprocess.check_output(["git-clang-format-7", "--binary", "clang-format-7", "--extensions", "cpp,h",  "--diff"])
+
+if output not in ['no modified files to format\n', 'clang-format did not modify any files\n']:
+    print "Run git clang-format, then commit.\n"
+    exit(1)
+else:
+    exit(0)
+    
