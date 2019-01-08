221538c0a382e50a4d5b23adefbd88d21220c883
Aurelien <aurelien.laporte@canaltp.fr>
2019-01-08 18:27:35 +0100

    Changing python script to bash script

---
+++ scripts/check-formatting.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+files='.*\.\(cpp\|hpp\|cc\|cxx\|h\)'
+
+output="$(diff -u <(find asgard/ -regex "$files" -exec cat {} \;) <(find asgard/ -regex "$files" -exec clang-format-7 -style=file {} \;))"
+
+if [ "$output" != "" ]
+then
+    echo "$output"
+    echo "The code is not correcly formatted."
+    echo "Run git clang-format, then commit."
+    exit 1
+fi
